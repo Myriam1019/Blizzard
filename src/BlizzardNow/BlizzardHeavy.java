76b5dcc0c94cda6d5a18ee3432dd584cc5d5eb5c
15718 <15718@LAPTOP-SS0766RE>
2021-12-01 16:14:51 -0500

    Freezing out...

--- src/BlizzardNow/BlizzardHeavy.java
+++ src/BlizzardNow/BlizzardHeavy.java
@@ -3,10 +3,12 @@ package BlizzardNow;
 public class BlizzardHeavy {
 
 	public static void main(String[] args) {
-		System.out.println("Its freezing out");
+		System.out.println("Its freezing out...");
 		
 		System.out.println("Heavy Snow...");
 		
-		System.out.println("Bizzard");
+		System.out.println("Blizzard...");
+		
+		System.out.println("Freezing out...");
 	}
 }
