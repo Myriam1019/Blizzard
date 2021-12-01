0a429910b713d481561f8597210d9cb9a815805a
15718 <15718@LAPTOP-SS0766RE>
2021-12-01 16:10:37 -0500

    Heavy snow... Blizzard...

--- src/BlizzardNow/BlizzardHeavy.java
+++ src/BlizzardNow/BlizzardHeavy.java
@@ -4,5 +4,9 @@ public class BlizzardHeavy {
 
 	public static void main(String[] args) {
 		System.out.println("Its freezing out");
+		
+		System.out.println("Heavy Snow...");
+		
+		System.out.println("Bizzard");
 	}
 }
